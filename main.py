d5638a170c20233831578318230c69ed99909825
JimmyThePhantom <dim.dalamagkas@gmail.com>
2021-10-18 23:10:02 +0300

    Final

--- main.py
+++
@@ -1,6 +0,0 @@
-import SimulationEnvironment as se
-
-canvas_width = 1100
-canvas_height = 600
-simulation = se.Simulation(
-    [canvas_width, canvas_height], population=70, agent_size=3, sick_population=3)
